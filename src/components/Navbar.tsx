4a960981367ea1bab5e6d529a22912e227fdbde8
Rachel <rachelcheework@gmail.com>
2025-07-06 23:50:34 +0800

    initial commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 // src/components/Navbar.tsx
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import '../index.css';
 
