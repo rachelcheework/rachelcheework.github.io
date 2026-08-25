079a580da735d5618cee7930e49dce60dc645ad6
Rachel <rachelcheework@gmail.com>
2026-08-25 19:49:37 +0800

    navbar fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,18 +15,18 @@ const Navbar = () => {
     }
 
   return (
-    <nav className="relative">
+    <nav>
       {/* Desktop Nav */}
-      <div className="hidden absolute top-40 md:flex md:flex-col p-3 space-y-2 rounded-lg border border-gray-500 text-white bg-black">
+      <div className="hidden fixed top-40 md:flex md:flex-col p-3 space-y-2 rounded-lg border border-gray-500 text-white bg-black">
         <Link to="/" className={`nav-button ${isActive('/')}`}>about me</Link>
         <Link to="/projects" className={`nav-button ${isActive('/projects')}`}>projects</Link>
         <Link to="/connect" className={`nav-button ${isActive('/connect')}`}>connect</Link>
       </div>
 
       {/* Hamburger */}
-      <div className="absolute top-10 right-10 md:hidden">
+      <div className="fixed top-10 right-10 z-50 md:hidden">
         <button
-          className={`z-40 block hamburger md:hidden focus:outline-none ${isOpen ? 'open' : ''}`}
+          className={`z-50 block hamburger md:hidden focus:outline-none ${isOpen ? 'open' : ''}`}
           onClick={navToggle}
         >
           <span className="hamburger-top"></span>
@@ -37,7 +37,7 @@ const Navbar = () => {
 
       {/* Mobile Menu */}
       <div
-        className={`flex absolute z-50 top-0 bottom-0 left-0 flex-col self-end w-full min-h-screen py-1 pt-36 px-12 space-y-6 text-md text-gray-800 bg-white/70 backdrop-blur-xl transition-all duration-700 ease-out
+        className={`fixed inset-0 z-20 flex flex-col w-full self-end min-h-screen py-1 pt-36 px-12 space-y-6 text-md text-gray-800 bg-white/70 backdrop-blur-xl transition-transform duration-700 ease-out
           ${isOpen ? 'translate-x-0' : 'translate-x-full'} md:hidden`}
       >
         <Link to="/" className={`nav-button ${isActive('/')}`} onClick={navToggle}>about me</Link>
